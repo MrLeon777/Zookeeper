88063f98bbe7c9771e64936c8b1a740a7f73bbe5
Your Name <you@example.com>
2020-09-07 15:51:32 +0300

    Initial commit

---
+++ Problems/Desks/main.py
@@ -0,0 +1,7 @@
+# put your python code here
+a = int(input())
+b = int(input())
+c = int(input())
+sum = a + b + c
+zero_five = (sum / 2) - (sum // 2)
+print(int(sum / 2 + zero_five))
---
+++ Problems/Lucky ticket/main.py
@@ -0,0 +1,7 @@
+# Save the input in this variable
+ticket = '090234'
+
+# Add up the digits for each half
+half1 = list(ticket[0:3])
+
+print(half1)
